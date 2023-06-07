2bab5cd6f8651cfc3a413e1e45c4bccb69166d9a
Sara Connelly <sara.k.connelly@gmail.com>
2023-06-07 16:19:02 -0600

    Initial commit

---
+++ plants.js
@@ -0,0 +1,9 @@
+var gets_sunshine = true;
+var ounces_of_water = 10;
+
+if (ounces_of_water >= 10 && gets_sunshine) {
+    console.log("This plant will thrive!");
+} else {
+    console.log("This plant will die.");
+}
+// Sara and Patrick are awesome. 
